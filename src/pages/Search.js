2002e2838ce7a0455ec2f384b75cc8c53e110f8c
Thais Rosa <tsrosa18@gmail.com>
2022-09-08 10:41:45 -0300

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,49 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    isButtonDisabled: true,
+    text: '',
+  };
+
+  onInputChange = (event) => {
+    const { value } = event.target;
+    const MIN_LENGTH = 2;
+    if (value.length >= MIN_LENGTH) {
+      this.setState({
+        text: value,
+        isButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        text: value,
+        isButtonDisabled: true,
+      });
+    }
+  };
+
   render() {
+    const { isButtonDisabled, text } = this.state;
     return (
-      <div>
+      <div data-testid="page-search">
         <Header />
-        <div data-testid="page-search">Search</div>
+        <form>
+          <input
+            value={ text }
+            name="text"
+            data-testid="search-artist-input"
+            type="text"
+            onChange={ this.onInputChange }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ isButtonDisabled }
+          >
+            Pesquisar
+
+          </button>
+        </form>
       </div>
     );
   }
