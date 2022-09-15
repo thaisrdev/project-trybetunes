d8b6e1a8634a1d1b6590ffc290e0f7f70578aab8
Thais Rosa <tsrosa18@gmail.com>
2022-09-15 01:32:49 -0300

    requisito 12

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Favorites extends Component {
+  state = {
+    isLoading: false,
+    tracklist: [],
+  };
+
+  componentDidMount() {
+    this.getFavoritesList();
+  }
+
+  getFavoritesList = async () => {
+    this.setState({ isLoading: true });
+    const favoritesList = await getFavoriteSongs();
+    this.setState({ tracklist: favoritesList, isLoading: false });
+  };
+
   render() {
+    const { isLoading, tracklist } = this.state;
     return (
-      <div>
+      <div data-testid="page-favorites">
         <Header />
-        <div data-testid="page-favorites" />
+        { isLoading ? <Loading /> : (
+          tracklist.map((element) => (
+            <MusicCard
+              key={ element.trackId }
+              trackName={ element.trackName }
+              artist={ element.artist }
+              previewUrl={ element.previewUrl }
+              remove={ this.removeSongFromList }
+            />
+          ))
+        )}
       </div>
     );
   }
 }
+
 export default Favorites;
